48e4fe69e56bfe0ac43e5edf6c90c0c0bd326748
Jiachen Li <jiachenl@ifp-39.ifp.uiuc.edu>
2019-10-04 15:20:52 -0500

    update

--- configs/retinanet_r101_fpn_1x.py
+++ configs/retinanet_r101_fpn_1x.py
@@ -54,7 +54,8 @@ test_cfg = dict(
     max_per_img=100)
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = 'data/coco/'
+#data_root = 'data/coco/'
+data_root = '/ifp/data/COCO/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [

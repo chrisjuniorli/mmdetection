92a72b2971d5d213ae911cc9cb9b13a7b9612cb9
Jiachen Li <jiachenl@ifp-39.ifp.uiuc.edu>
2019-10-04 11:53:50 -0500

    update

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -6,4 +6,4 @@ CONFIG=$1
 GPUS=$2
 
 $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS \
-    $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
+    tools/train.py $CONFIG --launcher pytorch ${@:3}

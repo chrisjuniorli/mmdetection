778a1196ba420452b36f4614a5834789cc7a670e
Jiachen Li <jiachenl@ifp-39.ifp.uiuc.edu>
2019-10-07 21:50:10 -0500

    update

---
+++ mmdet/models/anchor_heads/two_branches_head.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+from mmcv.cnn import normal_init
+
+from mmdet.core import distance2bbox, force_fp32, multi_apply, multiclass_nms
+from ..builder import build_loss
+from ..registry import HEADS
+from ..utils import ConvModule, Scale, bias_init_with_prob
+
+
+INF = 1e8

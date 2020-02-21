da1541475052c32ca08b4a5f4e0e03d1d8f49bfe
chrisjuniorli <chrisleesjtu@gmail.com>
2020-02-21 14:49:45 -0600

    update

--- tools/dist_train_fix_port.sh
+++ tools/dist_train_fix_port.sh
@@ -4,7 +4,7 @@ PYTHON=${PYTHON:-"python"}
 
 CONFIG=$1
 GPUS=$2
-PORT=${PORT:-29500}
+PORT=${PORT:-29000}
 
 $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     tools/train_fix.py $CONFIG --launcher pytorch ${@:3}
